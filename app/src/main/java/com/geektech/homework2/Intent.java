3451b88d21ccc09b1dd14eaf19d1dc8ca805f774
Dzhaparov Bekmamat Kanybekovich <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-03-13 13:57:57 +0600

    add some details

---
+++ app/src/main/java/com/geektech/homework2/Intent.java
@@ -0,0 +1,4 @@
+package com.geektech.homework2;
+
+public class Intent {
+}
--- app/src/main/java/com/geektech/homework2/MainActivity.java
+++ app/src/main/java/com/geektech/homework2/MainActivity.java
@@ -1,39 +1,55 @@
 package com.geektech.homework2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import android.annotation.SuppressLint;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private EditText editText2;
+    private TextView textView;
+    private TextView textView2;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText editText = findViewById(R.id.edit_text);
-        Button button = findViewById(R.id.btn);
+        editText = findViewById(R.id.edit_text);
+        editText2 = findViewById(R.id.edit_text_2);
+        button = findViewById(R.id.btn);
+        textView = findViewById(R.id.vviz);
+        textView2 = findViewById(R.id.zp);
 
-// Установите слушатель текста для EditText
+
+        button.setOnClickListener(view -> {
+            String login = editText.getText().toString();
+            String password = editText2.getText().toString();
+            if (login.equals("admin") && password.equals("admin")) {
+                Toast.makeText(MainActivity.this, "Вы успешно авторизовались", Toast.LENGTH_SHORT).show();
+                editText.setVisibility(View.GONE);
+                editText2.setVisibility(View.GONE);
+                button.setVisibility(View.GONE);
+                textView.setVisibility(View.GONE);
+                textView2.setVisibility(View.GONE);
+            } else {
+                Toast.makeText(MainActivity.this, "Неправильный логин или пароль", Toast.LENGTH_SHORT).show();
+            }
+        });
         editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // Не нужно ничего делать здесь
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // Если текст в EditText не пустой, измените цвет кнопки на красный, иначе на серый
-
-                //  button.setBackgroundColor(getResources().getColor(R.color.dark_grey));
                 if (!editText.getText().toString().equals("")) {
                     button.setBackgroundResource(R.drawable.rounded_object_orange);
                 } else {
